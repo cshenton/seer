94b68e1f181951fff6550b608a1c34f4c82a3285
cshenton <charlieshenton@me.com>
2018-02-14 09:52:12 +1100

    tested dist/mv/normal

--- dist/mv/normal.go
+++ dist/mv/normal.go
@@ -1,8 +1,31 @@
 package mv
 
+import "errors"
+
 // Normal is a multivariate normal distribution with full rank covariance.
 type Normal struct {
-	Dim        int
 	Location   []float64
 	Covariance []float64
+}
+
+// NewNormal constructs a Normal with the given location, covariance (if possible).
+func NewNormal(loc, cov []float64) (n *Normal, err error) {
+	if len(loc) == 0 {
+		err := errors.New("Location must have length 1 or greater")
+		return nil, err
+	}
+	if len(cov) != len(loc)*len(loc) {
+		err := errors.New("Covariance must have length equal to Location's squared length")
+		return nil, err
+	}
+	n = &Normal{
+		Location:   loc,
+		Covariance: cov,
+	}
+	return n, nil
+}
+
+// Dim returns the dimensionality of the mv Normal distribution.
+func (n *Normal) Dim() int {
+	return len(n.Location)
 }
---
+++ dist/mv/normal_test.go
@@ -0,0 +1,74 @@
+package mv_test
+
+import (
+	"testing"
+
+	"github.com/chulabs/seer/dist/mv"
+)
+
+func TestNewNormal(t *testing.T) {
+	tt := []struct {
+		name string
+		loc  []float64
+		cov  []float64
+	}{
+		{"1x1", []float64{1}, []float64{1}},
+		{"2x2", []float64{1, 1}, []float64{1, 0, 0, 1}},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			n, err := mv.NewNormal(tc.loc, tc.cov)
+			if err != nil {
+				t.Error("unexpected error in NewNormal:", err)
+			}
+			if n.Location[0] != tc.loc[0] {
+				t.Error("non matching input, output loc")
+			}
+		})
+	}
+}
+
+func TestNewNormalErrs(t *testing.T) {
+	tt := []struct {
+		name string
+		loc  []float64
+		cov  []float64
+	}{
+		{"empty location", []float64{}, []float64{1}},
+		{"mismatched lengths", []float64{1}, []float64{1, 1}},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			n, err := mv.NewNormal(tc.loc, tc.cov)
+			if err == nil {
+				t.Error("expected error, but it was nil")
+			}
+			if n != nil {
+				t.Error("expected nil pointer, but it was", n)
+			}
+		})
+	}
+}
+
+func TestDim(t *testing.T) {
+	tt := []struct {
+		name string
+		loc  []float64
+		cov  []float64
+		dim  int
+	}{
+		{"1x1", []float64{1}, []float64{1}, 1},
+		{"2x2", []float64{1, 1}, []float64{1, 0, 0, 1}, 2},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			n, err := mv.NewNormal(tc.loc, tc.cov)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if tc.dim != n.Dim() {
+				t.Errorf("expected Dim %v, but it was %v", tc.dim, n.Dim())
+			}
+		})
+	}
+}
