b4818c37f49b3ec4f87431cd68a2a933e8e071b1
cshenton <charlieshenton@me.com>
2018-03-04 14:40:56 +1100

    store error message tests

--- store/errors.go
+++ store/errors.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 )
 
-// NotFoundError is returned when the requested entity is not in the data store.
+// NotFoundError is returned when the requested Entity is not in the data store.
 type NotFoundError struct {
-	kind   string
-	entity string
+	Kind   string
+	Entity string
 }
 
 // Error message for NotFoundErrors, implements error interface.
 func (err *NotFoundError) Error() string {
-	return fmt.Sprintf("no %v with name %v was found in store", err.kind, err.entity)
+	return fmt.Sprintf("no %v with name %v was found in store", err.Kind, err.Entity)
 }
 
-// AlreadyExistsError is returned when an entity name is already taken.
+// AlreadyExistsError is returned when an Entity name is already taken.
 type AlreadyExistsError struct {
-	kind   string
-	entity string
+	Kind   string
+	Entity string
 }
 
 // Error message for AlreadyExistsError, implements error interface.
 func (err *AlreadyExistsError) Error() string {
-	return fmt.Sprintf("a %v with name %v already exists", err.kind, err.entity)
+	return fmt.Sprintf("a %v with name %v already exists", err.Kind, err.Entity)
 }
 
-// NoneFoundError is returned when a list request on a kind returns no entities.
+// NoneFoundError is returned when a list request on a Kind returns no entities.
 type NoneFoundError struct {
-	kind string
+	Kind string
 }
 
 // Error message for NoneFoundError, implements error interface.
 func (err *NoneFoundError) Error() string {
-	return fmt.Sprintf("no entities of kind %v were found", err.kind)
+	return fmt.Sprintf("no entities of kind %v were found", err.Kind)
 }
