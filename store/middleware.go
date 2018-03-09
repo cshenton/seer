8325f8533d8a994eaef23db2e1e72c1394fb5505
cshenton <charlieshenton@me.com>
2018-03-09 23:08:38 +1100

    tests for store access via context

--- store/middleware.go
+++ store/middleware.go
@@ -2,11 +2,7 @@ package store
 
 import "context"
 
-// BadgerMiddleware appends the badger implementation of the store interfaces
-// onto the current request context.
-func BadgerMiddleware(c context.Context) context.Context {
-	type tempString string
-	return context.WithValue(c, tempString("foo"), "bar")
-	// do this once interface implemented
-	// return streamToContext(c, s)
+// StreamMiddleware appends the provided StreamStore to the request context.
+func StreamMiddleware(c context.Context, s StreamStore) context.Context {
+	return streamToContext(c, s)
 }
