ff4de6a00ad7df4ad7af65a19de5e044b63abf09
cshenton <charlieshenton@me.com>
2018-03-10 13:11:09 +1100

    server.New and test

---
+++ server/server_test.go
@@ -0,0 +1,31 @@
+package server_test
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/chulabs/seer/server"
+)
+
+func testPath(t *testing.T) string {
+	f, err := ioutil.TempFile(os.TempDir(), "bolt_test")
+	if err != nil {
+		t.Fatal("failed to create test db file")
+	}
+	return f.Name()
+}
+
+func TestNew(t *testing.T) {
+	_, err := server.New(testPath(t))
+	if err != nil {
+		t.Fatal("unexpected error in server.New:", err)
+	}
+}
+
+func TestNewErrs(t *testing.T) {
+	_, err := server.New("/$$$NOPE!!")
+	if err == nil {
+		t.Error("expected error, but it was nil")
+	}
+}
